7ea4021b2e3d96f25be1b18b3746f94579fee593
diana2886 <tdm2886@gmail.com>
2022-04-30 17:49:01 +0600

    feat: add Key.js

---
+++ virtual-keyboard/src/js/Key.js
@@ -0,0 +1,19 @@
+import createDomNode from "./CreateDomNode";
+
+export default class Key {
+  constructor({ key, shift, code }) {
+    this.key = key;
+    this.shift = shift;
+    this.code = code;
+    this.isFnKey = Boolean(small.match(/Tab|Caps|Shift|Ctrl|Alt|Enter|Back|Del|arr|Win/));
+    
+    if (shift && shift.match(/[^0-9a-zA-Zа-яА-ЯёЁ]/)) {
+      this.keyShift = createDomNode('div', 'keyShift', this.shift);
+    } else {
+      this.keyShift = createDomNode('div', 'key-shift', '');
+    }
+
+    this.keyTitle = createDomNode('div', 'key-title', key);
+    this.keyContainer = createDomNode('div', 'keyboard__key', [this.keyShift, this.keyTitle], null, ['code', this.code]);
+  }
+}
