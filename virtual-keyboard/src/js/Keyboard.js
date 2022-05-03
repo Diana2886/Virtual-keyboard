1cc68cb405b2bdd486a2b2d96fe898dff666daa9
diana2886 <tdm2886@gmail.com>
2022-05-03 08:35:28 +0600

    feat: add changing language and animation for keys

--- virtual-keyboard/src/js/Keyboard.js
+++ virtual-keyboard/src/js/Keyboard.js
@@ -36,5 +36,51 @@ export default class Keyboard {
         }
       })
     });
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
+  }
+
+  handleEvent = (event) => {
+    // if (event.stopPropagation) event.stopPropagation();
+    const { code, type } = event;
+    const keyObject = this.keyButtons.find(key => key.code === code);
+    if (!keyObject) return;
+    this.display.focus();
+    if (type.match(/keydown|mousedown/)) {
+      if (type.match(/key/)) event.preventDefault();
+      keyObject.keyContainer.classList.add('active');
+
+      if (code.match(/Control/)) this.ctrlKey = true;
+      if (code.match(/Alt/)) this.altKey = true;
+      if (code.match(/Control/) && this.altKey) this.changeLang();
+      if (code.match(/Alt/) && this.ctrlKey) this.changeLang();
+
+    } else if (type.match(/keyup|mouseup/)) {
+      keyObject.keyContainer.classList.remove('active');
+      if (code.match(/Control/)) this.ctrlKey = false;
+      if (code.match(/Alt/)) this.altKey = false;
+    }
+  }
+
+  changeLang = () => {
+    const langs = Object.keys(lang);
+    let langIndex = langs.indexOf(this.container.dataset.lang);
+    this.keyBase = langIndex + 1 < langs.length ? lang[langs[langIndex += 1]]
+      : lang[langs[langIndex -= langIndex]];
+    this.container.dataset.lang = langs[langIndex];
+    storage.set('lang', langs[langIndex]);
+    
+    this.keyButtons.forEach(item => {
+      const keyObject = this.keyBase.find(key => key.code === item.code);
+      if (!keyObject) return;
+      item.shift = keyObject.shift;
+      item.key = keyObject.key;
+      if (keyObject.shift && keyObject.shift.match(/[^0-9a-zA-Zа-яА-ЯёЁ]/g)) {
+        item.keyShift.innerHTML = keyObject.shift;
+      } else {
+        item.keyShift.innerHTML = '';
+      }
+      item.keyTitle.innerHTML = keyObject.key;
+    });
   }
 }
